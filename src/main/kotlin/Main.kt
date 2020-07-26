b0fca2ed89fd187d78856bab3c7bdfd21cc7b75b
nylanderdev <rasmus.s.nylander@gmail.com>
2020-07-26 17:20:34 +0200

    Add JavaFX boilerplate

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
-fun main() {
-    println("Hello world!");
+import javafx.application.Application;
+import javafx.stage.Stage
+
+class Main : Application() {
+
+    fun launchApp(args: Array<String>) {
+        launch(*args)
+    }
+
+    override fun start(primary: Stage) {
+        primary.title = "Hello World!"
+        primary.width = 600.0
+        primary.height = 400.0
+        primary.show()
+    }
+}
+
+fun main(args: Array<String>) {
+    Main().launchApp(args)
 }
