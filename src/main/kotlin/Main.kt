a54c4c345e70ede2ad816b77c1e35e3c75408249
nylanderdev <rasmus.s.nylander@gmail.com>
2020-07-29 15:07:16 +0200

    Separate application class from Main entrypoint script

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,3 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader
-import javafx.scene.Scene
-import javafx.stage.Stage
-
-class Main : Application() {
-
-    companion object {
-        private const val PLAIN_TEXT_EDITOR: String = "PlainTextEditor"
-
-        private fun loadFxml(name: String): Scene {
-            return Scene(FXMLLoader.load(javaClass.getResource("fxml/$name.fxml")))
-        }
-    }
-
-    fun launchApp(args: Array<String>) {
-        launch(*args)
-    }
-
-    override fun start(primary: Stage) {
-        primary.title = "Bokstavligen"
-        primary.width = 600.0
-        primary.height = 400.0
-        primary.scene = loadFxml(PLAIN_TEXT_EDITOR)
-        primary.show()
-    }
-
-}
-
 fun main(args: Array<String>) {
-    Main().launchApp(args)
+    Bokstavligen().launchApp(args)
 }
