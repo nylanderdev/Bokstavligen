24ce4daf742ad29fd175640c13a946adac3c5b90
nylanderdev <rasmus.s.nylander@gmail.com>
2020-08-02 09:29:11 +0200

    Implement word index

--- src/main/kotlin/extension/TextAreaExtensions.kt
+++ src/main/kotlin/extension/TextAreaExtensions.kt
@@ -4,13 +4,21 @@ import javafx.scene.control.TextArea
 
 fun TextArea.caretRowAndColumn(): Pair<Int, Int> {
     // todo (low priority): O(n), optimize if necessary and possible
-    val textBeforeCaret = this.text.substring(0, this.caretPosition)
+    val textBeforeCaret = this.textBeforeCaret()
     val caretLineStartIndex = textBeforeCaret.lastIndexOf("\n") + 1
     val caretLineBeforeCaret = textBeforeCaret.substring(caretLineStartIndex)
     val row = countNewLines(textBeforeCaret)
     val column = caretLineBeforeCaret.length
     return Pair(row, column)
 }
+
+fun TextArea.caretWordIndex(): Int {
+    val textBeforeCaret = this.textBeforeCaret()
+    return textBeforeCaret.wordCount()
+}
+
+private fun TextArea.textBeforeCaret(): String =
+        this.text.substring(0, this.caretPosition.coerceAtMost(this.text.length))
 
 private fun countNewLines(string: String): Int =
         string.length - string.replace("\n", "").length
